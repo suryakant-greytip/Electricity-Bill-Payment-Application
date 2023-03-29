dbf3fc8dd53d10e28e69b0650481cbfd188448d8
clrsurya11 <singhsuryakant9011@gmail.com>
2023-03-29 19:46:10 +0530

    Initial commit

---
+++ Electricity-Bill-Payment-System/src/com/paytric/ui/MainUI.java
@@ -0,0 +1,17 @@
+package com.paytric.ui;
+
+import com.paytric.colors.UIColors;
+
+public class MainUI {
+
+	public static void main(String[] args) {
+	
+		System.out.println("              "+UIColors.BLACK_BOLD+"_________________________________________________________");
+		System.out.println("             "+UIColors.BLACK_BOLD+"|"+UIColors.TEAL_BACKGROUND+"                                                         "+UIColors.RESET+UIColors.BLACK_BOLD+"|");
+		System.out.println("             "+UIColors.BLACK_BOLD+"|"+UIColors.TEAL_BACKGROUND+"  "+UIColors.BLACK_BOLD+"Welcome to "+UIColors.DARK_BLUE+"PAYTRIC"+UIColors.RESET+UIColors.TEAL_BACKGROUND+UIColors.BLACK_BOLD+" - Electricity Bill Payment System   "+UIColors.RESET+UIColors.BLACK_BOLD+"|");
+		System.out.println("             "+UIColors.BLACK_BOLD+"|"+UIColors.TEAL_BACKGROUND+"_________________________________________________________"+UIColors.RESET+UIColors.BLACK_BOLD+"|");
+		
+		
+		
+	}
+}
